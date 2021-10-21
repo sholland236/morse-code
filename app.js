0d05020923cb259292576d2a09504830264adaf2
Sarah Holland <sholland236@gmail.com>
2021-10-21 12:24:48 +0100

    event listener for translateFromMorse

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ import translateFromMorse from "./translate-from-morse";
 const input = document.querySelector("#englishText");
 const output = document.querySelector("#output");
 const translateButton = document.querySelector("#translate");
+const morseInput = document.querySelector("#morseText");
+const morseOutput = document.querySelector("#morseOutput");
+const morseTranslate = document.querySelector("#morseTranslate");
 
 const performTranslation = () => {
     output.innerHTML = translate(input.value);
 }
 translateButton.addEventListener("click", performTranslation);
 
+const performMorseTranslation = () => {
+    morseOutput.innerHTML = translateFromMorse(morseInput.value);
+}
+morseTranslate.addEventListener("click", performMorseTranslation);
